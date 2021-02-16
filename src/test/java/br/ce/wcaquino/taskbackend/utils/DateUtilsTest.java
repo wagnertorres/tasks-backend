1f09ad6963cb11231544c9fac8cf5eea4824e6fa
Wagner Torres <wstorres@gmail.com>
2021-02-16 16:00:48 -0300

    Voltando ao código correto

--- src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
+++ src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
@@ -10,7 +10,7 @@ public class DateUtilsTest {
     @Test
     public void deveRetornarTrueParaDatasFuturas(){
         LocalDate localDate = LocalDate.of(2030, 01, 01);
-        Assert.assertFalse(DateUtils.isEqualOrFutureDate(localDate));
+        Assert.assertTrue(DateUtils.isEqualOrFutureDate(localDate));
     }
 
     @Test
